b9bce890adaca4ec9be1366e26230cc3d01dea4e
PraneethGunda <61630288+Praneeth73@users.noreply.github.com>
2026-08-20 20:07:39 -0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if (intervals.isEmpty()) return true;
+        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+
+       // boolean result = true;
+
+        int last = intervals.get(0).end;
+
+        for(int i=1 ; i<intervals.size(); i++){
+            if(last > intervals.get(i).start){
+                return false;
+            }else{
+                last = intervals.get(i).end;
+            }
+        }
+        return true;
+
+    }
+}
